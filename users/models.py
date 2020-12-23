14f6250e15aed9d3e84c11cc6b8bf2b0d8542419
Cristian <desancheztorres@gmail.com>
2020-12-23 17:02:56 +0000

    Model methods

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 class User(models.Model):
@@ -23,18 +24,26 @@ class Website(models.Model):
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ['rating']
-        db_table = 'website_custom_table_name'
-        verbose_name = 'the Website'
-        verbose_name_plural = 'the websites'
+    def was_released_last_week(self):
+        if self.release_date < datetime.date(2019, 3, 3):
+            return "Released before last week"
+        else:
+            return "Released this week"
+
+    @property
+    def get_full_name(self):
+        return f"This is my website's full name: {self.name}"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f"/websites/{self.id}"
+
+    def save(self, *args, **kwargs):
+        print("We are doing something else here...")
+        super().save(*args, **kwargs)
 
 
 class Car(models.Model):
     name = models.CharField(max_length=40, primary_key=True)
-
-
-class Ox(models.Model):
-    class Meta:
-        verbose_name = 'ox'
-        verbose_name_plural = 'oxen'
